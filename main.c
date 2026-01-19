77e87ddb20f2f769851f39f05fd46f608ac70953
Markus <abramianmedina@gmail.com>
2026-01-19 19:02:52 +0000

    mi first program

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+
+    // This is my first program!
+
+    /*
+    This 
+    is
+    also 
+    comment
+    */
+
+    printf("Hello world!\n");
+    printf("Im so good!");
+    
+    return 0;
+}
